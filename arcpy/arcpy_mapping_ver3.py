25b814f43781a6c4082025be622b00e34b70e2aa
Yang Li <leeyang1991@gmail.com>
2019-06-25 21:36:49 +0800

    Add files via upload

---
+++ arcpy/arcpy_mapping_ver3.py
@@ -0,0 +1,78 @@
+# coding=utf-8
+import os
+import arcpy
+import time
+import codecs
+
+this_root = os.getcwd()+'\\..\\'
+
+output_mxd = {'zhuanbian':'zhuanbian',
+              'naizhang_ganta':'naizhang_ganta',
+              'xiangshi_biandianzhan':'xiangshi_biandianzhan',
+              'duanluqi':'duanluqi',
+              'gongbian':'gongbian',
+              'dwg_Polyline':'***********',
+              'line_annotation':'line_annotation',
+              'zoom_layer':'zoom_layer'
+               }
+
+def mk_dir(dir):
+    if not os.path.isdir(dir):
+        os.makedirs(dir)
+
+
+def mapping(dir):
+    # 横竖选择
+    f=open(dir+'\\'+'config.txt','r')
+    line = f.readline()
+    if line == 'heng':
+        # mxd_file = 'D:\\project13\\heng_ver5.mxd'
+        mxd_file = this_root+'mxd\\template_heng.mxd'
+    elif line == 'shu':
+        mxd_file = this_root + 'mxd\\template_shu.mxd'
+    else:
+        mxd_file = None
+    # mxd_file = 'D:\\project13\\新制作shu.mxd'
+    mxd = arcpy.mapping.MapDocument(mxd_file)
+    df0 = arcpy.mapping.ListDataFrames(mxd)[0]
+
+    workplace = 'SHAPEFILE_WORKSPACE'
+    output_mxd['dwg_Polyline'] = (dir.split('\\')[-2]+'_dwg_Polyline').decode('gbk')
+    # print (dir.split('\\')[-2]+'_dwg_Polyline').decode('gbk')
+    # exit()
+    for i in output_mxd:
+
+        try:
+            lyr = arcpy.mapping.ListLayers(mxd, i, df0)[0]
+            lyr.replaceDataSource(dir,workplace,output_mxd[i])
+            print '绘制完成'.decode('gbk') + i.decode('gbk')
+        except:
+            print 'no '+i.decode('gbk')
+
+    extent_lyr = arcpy.mapping.ListLayers(mxd,output_mxd['zoom_layer'],df0)[0]
+    df0.extent = extent_lyr.getSelectedExtent()
+
+    # dir = dir.encode('gbk')
+    print 'saving mxd to',(dir+'\\mxd.mxd').decode('gbk')
+    dir = dir.decode('gbk')
+    if os.path.isfile(dir+'\\mxd.mxd'):
+        os.remove(dir+'\\mxd.mxd')
+    mxd.saveACopy(dir+'\\mxd.mxd','9.2')
+    mk_dir(this_root+'output_pic\\')
+    outjpeg = this_root+'output_pic\\'+dir.split('\\')[-2]
+
+    arcpy.mapping.ExportToJPEG(mxd,outjpeg,data_frame='PAGE_LAYOUT',df_export_width=mxd.pageSize.width,df_export_height=mxd.pageSize.height,color_mode='24-BIT_TRUE_COLOR',resolution=300,jpeg_quality=100)
+    print 'done'
+
+
+def main():
+
+    dir = this_root+'190509\\民权线路cad\\dwg_to_shp\\10kV鲁10Ⅱ鲁西线\\'
+    mapping(dir)
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
+
---
+++ arcpy/dwg_to_shp.py
@@ -0,0 +1,66 @@
+# coding=utf-8
+
+import arcpy
+import os
+import log_process
+import time
+
+this_root = os.getcwd()+'\\..\\'
+
+# in_features_point = this_root+'new_test_data\\123.dwg\\Annotation'
+# in_features_line = this_root+'new_test_data\\123.dwg\\Polyline'
+
+def mk_dir(dir):
+    if not os.path.isdir(dir):
+        os.makedirs(dir)
+# 10kV丹1丹常线
+
+def dwg_to_shp(in_features, output_dir):
+    arcpy.FeatureClassToShapefile_conversion(in_features, output_dir)
+
+
+def check():
+    fdir = this_root + u'190509\\民权线路cad\\dwg_to_shp\\'
+    flist = os.listdir(fdir)
+    for folder in flist:
+        file_count = len(os.listdir(fdir+folder))
+        if file_count != 12:
+            print(folder)
+    pass
+
+def run_dwg_to_shp():
+    log = log_process.Logger('log.log')
+    fdir = this_root+u'190509\\35kV线路\\35kV线路\\'
+    flist = os.listdir(fdir)
+
+    init_time = time.time()
+    flag = 0
+    for f in flist:
+        start = time.time()
+        out_dir = this_root+u'190509\\35kV线路\\dwg_to_shp\\'+f.split('.')[0]
+        if os.path.isdir(out_dir):
+            flag += 1
+            continue
+        mk_dir(out_dir)
+        try:
+            dwg_to_shp(fdir+f+'\\Annotation',out_dir)
+            dwg_to_shp(fdir+f+'\\Polyline',out_dir)
+            log.logger.info('\\Annotation')
+            log.logger.info('\\Polyline')
+        except Exception as e:
+            log.logger.error(e)
+        end = time.time()
+        log_process.process_bar(flag,len(flist),init_time,start,end,str(flag+1)+'/'+str(len(flist))+'\n')
+        flag += 1
+        # print(fdir+f)
+    # dwg_to_shp(in_features_line,this_root)
+
+
+def main():
+    run_dwg_to_shp()
+
+if __name__ == '__main__':
+    main()
+
+
+
